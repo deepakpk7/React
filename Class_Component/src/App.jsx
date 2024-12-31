da914c4507b21896042870bbcf00570f776a6182
deepakpk7 <deepakpk7034@gmail.com>
2024-12-31 13:30:41 +0530

    update

---
+++ Class_Component/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <h2>Welcome Rect Class Component</h2>
+//     )
+//   }
+// }
+class App extends React.Component{
+  constructor(props){
+    super()   
+    this.state={
+      color:'red',
+      size:'400',
+      name:props.name,
+    }
+  }
+  render(){
+    return(
+    <div>
+      <h1>Welcome Rect Class Component</h1>
+      <p>{this.state.color}</p>
+      <p>{this.state.size}</p>
+      <p>{this.state.name}</p>
+
+    </div>
+    )
+  }
+}
+export default App
+
+
